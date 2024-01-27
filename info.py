8f38562c628c38cd8c9047dab7e81f8ea6cdceee
Mr Sagar <143385461+MrSagarBots@users.noreply.github.com>
2024-01-27 20:44:55 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,7 @@
+API_ID       = 
+API_HASH     = ""
+BOT_TOKEN    = ""
+SESSION      = ""
+DATABASE_URI = ""
+LOG_CHANNEL  = 
+ADMIN        = 
---
+++ utils/script.py
@@ -0,0 +1,56 @@
+class script(object):
+    START = """<b>**Hello {} 🤟**
+   
+I am **ᴘᴏsᴛ sᴇᴀʀᴄʜ ʙᴏᴛ**.I am best Channel Link Search Bot! 
+I Will filter your channel posts automatically and send it in your group chat when Someone search it.
+For More Info Click /help</b>"""
+
+    HELP = """<b>‼️ ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴍᴇ ɪɴ ᴀ ɢʀᴏᴜᴘ ‼️
+
+
+❂ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ & ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ᴀʟʟ ᴘᴇʀᴍɪꜱꜱɪᴏɴꜱ..
+
+❂ ꜱᴇɴᴅ /verify ɪɴ ɢʀᴏᴜᴘ & ᴡᴀɪᴛ ꜰᴏʀ ɪᴛ ᴛᴏ ᴀᴄᴄᴇᴘᴛ ᴏʀ ᴅɪʀᴇᴄᴛʟʏ ᴄᴏɴᴛᴀᴄᴛ ᴛᴏ ᴏᴡɴᴇʀ ᴀꜰᴛᴇʀ ʀᴇQᴜᴇꜱᴛ @Talk_To_Developer_bot
+
+❂ ᴀꜰᴛᴇʀ ᴠᴇʀɪꜰɪᴄᴀᴛɪᴏɴ... ꜱᴇɴᴅ /connect ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪᴅ
+
+⤨ ᴇxᴀᴍᴘʟᴇ -
+
+/connect -100xxxxxxxxxx
+
+
+⤨ ʀᴇᴍᴏᴠᴇ ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ
+/disconnect -100xxxxxxxxxxx
+ᴛʜɪꜱ ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ʀᴇᴍᴏᴠᴇ ᴀ ɪɴᴅᴇxᴇᴅ ᴄʜᴀɴɴᴇʟ ꜰʀᴏᴍ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
+
+❂ ɢᴇᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟꜱ ʟɪꜱᴛ ᴡɪᴛʜ
+/connections
+
+ᴛᴏ ᴀᴅᴅ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴀᴅᴅ ᴛʜɪs ʙᴏᴛ ɪɴ ᴄʜᴀɴɴᴇʟ ɪɴ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴀᴅᴅ ᴀs ᴀ ꜰᴏʀᴄᴇ sᴜʙ.
+
+ᴛʜᴇɴ ɢᴇᴛ ɪᴛ's ɪᴅ ( ᴄʜᴀɴɴᴇʟ ɪᴅ) ᴀɴᴅ sᴇɴᴅ  /fsub ᴡɪᴛʜ ɪᴅ
+
+⤨ ᴇxᴀᴍᴘʟᴇ -
+
+/fsub -100xxxxxxxxxx</b>"""
+
+    ABOUT = """
+✯ Mʏ Nᴀᴍᴇ:  {}
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/MrSagarBots'>MrSagarBots</a>
+✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
+✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
+✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
+
+    STATS = """My Status 💫
+
+👥 Users: {}
+🧿 Groups: {}"""
+
+    BROADCAST = """<u>{}</u>
+
+Total: `{}`
+Remaining: `{}`
+Success: `{}`
+Failed: `{}`"""
+
+    
