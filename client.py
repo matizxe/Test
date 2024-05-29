3bb0b462618183bdca0cc7d3362a87368607c2bb
matizxe <129849674+matizxe@users.noreply.github.com>
2024-05-29 20:26:53 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,27 +1,35 @@
-import asyncio
+from info import *
 from pyrogram import Client
-from info import API_ID, API_HASH, BOT_TOKEN, SESSION, DATABASE_URI
-from plugins import *
-from utils.helpers import run_check_up
+from subprocess import Popen
+from utils.helpers import (
+    add_group, get_group, update_group, delete_group, delete_user, get_groups, add_user,
+    get_users, save_dlt_message, get_all_dlt_data, delete_all_dlt_data, search_imdb,
+    force_sub, broadcast_messages
+)
 
-class Bot(Client):
+User = Client(name="user", session_string=SESSION)
+DlBot = Client(name="auto-delete", 
+               api_id=API_ID,
+               api_hash=API_HASH,           
+               bot_token=BOT_TOKEN)
+
+class Bot(Client):   
     def __init__(self):
-        super().__init__(
-            "DlBot",
+        super().__init__(   
+           "bot",
             api_id=API_ID,
-            api_hash=API_HASH,
+            api_hash=API_HASH,           
             bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins")
-        )
-
-    async def start(self):
-        await super().start()
-        print("Bot started.")
-        asyncio.create_task(run_check_up())
-
+            plugins={"root": "plugins"})
+    
+    async def start(self):                        
+        await super().start()        
+        await User.start()
+        Popen("python3 -m utils.delete", shell=True)       
+        print("Bot Started 👍🙂")   
+    
     async def stop(self, *args):
         await super().stop()
-        print("Bot stopped.")
 
 if __name__ == "__main__":
     bot = Bot()
