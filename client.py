663e2cb61599c64948386df060aeaada403d7d51
matizxe <129849674+matizxe@users.noreply.github.com>
2024-05-29 20:13:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,33 +1,27 @@
-from info import *
-from pyrogram import Client
-from subprocess import Popen
 import asyncio
-
-User = Client(name="user", session_string=SESSION)
-DlBot = Client(name="auto-delete",
-               api_id=API_ID,
-               api_hash=API_HASH,
-               bot_token=BOT_TOKEN)
+from pyrogram import Client, idle
+from info import API_ID, API_HASH, BOT_TOKEN, SESSION
+from plugins import *
+from utils.helpers import run_check_up
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            "bot",
+            "DlBot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins={"root": "plugins"}
+            plugins=dict(root="plugins")
         )
 
     async def start(self):
         await super().start()
-        await User.start()
+        print("Bot started.")
         asyncio.create_task(run_check_up())
-        print("Bot Started 👍🙂")
 
     async def stop(self, *args):
         await super().stop()
-        await User.stop()
+        print("Bot stopped.")
 
 if __name__ == "__main__":
     bot = Bot()
