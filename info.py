91e806ddebff03df4ec185140550afc680c24a82
matizxe <129849674+matizxe@users.noreply.github.com>
2024-06-16 11:24:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,14 +1,14 @@
 from urllib.parse import quote_plus
 
-API_ID = 20551282
-API_HASH = "f2aa48726d861f983ddb5f8f8d044a1e"
-BOT_TOKEN = "7018018136:AAFJ3xHuNBRmSXGRm4_0_M1HLvvLYFYMTDA"
-SESSION = "BQE5lnIAj4Iynun3rVc-mDGYVFgwy_ofWDswx9zRHUG1KRWmHAtQjmrGS8oLIKiLdfYU8TA7iGWUZEpjNK2jEIDHCD_rBYmCb1keX9shflfp8W9UPN3C92LN48iVbh0q-gsBE4pAlagYVzcz9_7n6-LveWCut0GKqinI3mWGluKk2ADmRfIeOCouFNJC-lwIi09jNZnvBed1nTs3XoF1JZbYfCzarzJP4C9d7P2UuwoDKEt2YnhZ13KnM5wCXB-7afY4GTXGT4W1mLJFIlBVWoCko0N1mU3bGffkeKHTfcFIXm4JyykoXDsMUeDPIWUhK3DXfY4YcMx_cCK77DKLuHUiAD_qtQAAAAGJwFOWAA"
-LOG_CHANNEL = -1002110630687
-ADMIN = 6606050198
+API_ID = 21876230
+API_HASH = "14670d68563c80fd3bec3ae5d6321799"
+BOT_TOKEN = "7387418474:AAF5hER8zk4n2ZciKBjO8E7Vxoenuxg1FHc"
+SESSION = "BQFNzgYAScLVay5teAcN6L6MJPizWM_BN_xCtmXT9UmYbmlcrVG0JAA7yyBBCHZ_VGZ707vIc80dRcpC9V5UtDyU3kK4NujOBGK3sYDkIvMN9qo8oOQedo7Arh6TXtxecyjn9VVdYXRZklaieEpYdry_eB41jSw8HoCm9QNr2IEbN6JABLA4f0UeVK7NPM2MZh6byBbpCws6Y4bKt-SSckZlNQ_DNUAscjzklWAbOv36zt8yOUnft9TE72xlNQoTrNIfTp3AxdzjAiWPRLEaSMtB_5tFW8MmDjynMEQmGBGR1449zwH0euxITaPcDfhU5uTmy5hu4hYdpqZlxmrx5WO8vjHVBwAAAAF5wnctAA"
+LOG_CHANNEL = -1002218435383
+ADMIN = 6337754925
 
 # URL-encode the username and password
-username = quote_plus("forpaiduser")
-password = quote_plus("EXy9d9MqZznOSc0x")
+username = quote_plus("meoww903")
+password = quote_plus("y1gaA3YSHTXmrR4h")
 
-DATABASE_URI = f"mongodb+srv://{username}:{password}@cluster0.hd6xbja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = f"mongodb+srv://{username}:{password}@cluster0.fdcjjw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
